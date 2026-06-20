3497c588aa463317967a88c5f55ab657f9cd4aaa
Uttam1916 <tired3120@gmail.com>
2026-06-20 19:41:06 +0530

    fix: singular go.mod, go generate, replace linux/bpf

--- internal/filter/generate.go
+++
@@ -1,2 +0,0 @@
-package main
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang bpf ../../bpf/xdp_filter.bpf.c -- -I/usr/include
--- internal/filter/go.mod
+++
@@ -1,7 +0,0 @@
-module bpf
-
-go 1.25.0
-
-require github.com/cilium/ebpf v0.21.0
-
-require golang.org/x/sys v0.46.0
