67884aae7b48350badd3515766203fc6e9c4438f
Uttam1916 <tired3120@gmail.com>
2026-07-09 13:14:48 +0530

    feat: establish sslptr->connection relation

--- bpf/ssl_hook.bpf.c
+++ bpf/ssl_hook.bpf.c
@@ -4,9 +4,9 @@
 #include <bpf/bpf_core_read.h>
 
 #define MAX_BUF_SIZE 8192
-#define DIR_SEND  0 
-#define DIR_RECV  1
-#define DIR_CLOSE 2 
+#define DIR_SEND 0
+#define DIR_RECV 1
+#define DIR_CLOSE 2
 
 // each buf read and obtained
 struct ssl_buf {
@@ -16,6 +16,10 @@ struct ssl_buf {
     __u32 len;
     __u32 dir;
     __u64 ssl_ptr;
+    __u32 clientip;
+    __u32 serverip;
+    __u16 clientport;
+    __u16 serverport;
     __u8 buf[MAX_BUF_SIZE];
 };
 
@@ -30,16 +34,48 @@ struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 10240);
     __type(key, __u32);
-    __type(value, struct ssl_state);
+    __type(value, __u32);
 } bufs SEC(".maps");
 
+// for copying socket onto hashmap
+struct accept_state {
+    __u64 sockaddr_ptr;
+};
 
+struct conn_info {
+    __u32 client_ip;
+    __u16 client_port;
+};
+
+// map tid to socketaddr
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, __u32); // tid
+    __type(value, struct accept_state);
+} accept_state_map SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, int); // fd
+    __type(value, struct conn_info);
+} fd_to_conn SEC(".maps");
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 10240);
+    __type(key, __u64); // sslptr
+    __type(value, struct conn_info);
+} ssl_to_conn SEC(".maps");
+
+// ringbuffer to contain all plaintext
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
     __uint(max_entries, 8192 * 128);
 } ringbuf SEC(".maps");
 
-//Only capture nginx traffic
+// Only capture nginx traffic
 static __always_inline int is_target(void) {
     char comm[16];
     bpf_get_current_comm(&comm, sizeof(comm));
@@ -217,4 +253,69 @@ int BPF_URETPROBE(ssl_write_exit) {
     return 0;
 }
 
+// attach to kernel syscall for accept
+SEC("kprobe/__x64_sys_accept")
+int BPF_KPROBE(accept_enter, int sockfd, struct sockaddr *upeer_sockaddr,
+               int *upeer_addrlen) {
+    __u32 tid = (__u32)bpf_get_current_pid_tgid();
+
+    struct accept_state st = {
+        .sockaddr_ptr = (__u64)upeer_sockaddr,
+    };
+
+    // tid-> socket
+    bpf_map_update_elem(&accept_state_map, &tid, &st, BPF_ANY);
+
+    return 0;
+}
+
+SEC("kretprobe/__x64_sys_accept")
+int BPF_KRETPROBE(accept_exit) {
+    __u32 tid = (__u32)bpf_get_current_pid_tgid();
+
+    struct accept_state *st = bpf_map_lookup_elem(&accept_state_map, &tid);
+
+    if (!st)
+        return 0;
+
+    int newfd = PT_REGS_RC(ctx);
+
+    if (newfd < 0)
+        goto cleanup;
+
+    struct sockaddr_in addr = {};
+
+    if (bpf_probe_read_user(&addr, sizeof(addr), (void *)st->sockaddr_ptr))
+        goto cleanup;
+
+    struct conn_info conn = {};
+
+    conn.client_ip = addr.sin_addr.s_addr;
+    conn.client_port = addr.sin_port;
+
+    // fd-> connection
+    bpf_map_update_elem(&fd_to_conn, &newfd, &conn, BPF_ANY);
+
+cleanup:
+    bpf_map_delete_elem(&accept_state_map, &tid);
+
+    return 0;
+}
+
+SEC("uprobe/SSL_set_fd")
+int BPF_UPROBE(ssl_set_fd, void *ssl, int fd) {
+    struct conn_info *conn = bpf_map_lookup_elem(&fd_to_conn, &fd);
+
+    if (!conn)
+        return 0;
+
+    __u64 ssl_key = (__u64)ssl;
+
+    // sslptr-> connection which can later be used in return probes
+    bpf_map_update_elem(&ssl_to_conn, &ssl_key, conn, BPF_ANY);
+
+    bpf_map_delete_elem(&fd_to_conn, &fd);
+
+    return 0;
+}
 char LICENSE[] SEC("license") = "GPL";
