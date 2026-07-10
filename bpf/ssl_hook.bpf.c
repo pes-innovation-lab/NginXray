ba4850f489287ac804eeef0ab45d77a4bb1e2f09
Uttam1916 <tired3120@gmail.com>
2026-07-10 11:40:03 +0530

    chore: add docker comm in makefile & update comments

--- bpf/ssl_hook.bpf.c
+++ bpf/ssl_hook.bpf.c
@@ -9,6 +9,7 @@
 #define DIR_RECV 1
 #define DIR_CLOSE 2
 
+// buffer containing information that will be pushed to ringbuffer
 struct ssl_buf {
     __u64 timens;
     __u32 tid;
@@ -22,12 +23,13 @@ struct ssl_buf {
     __u16 server_port;
     __u8 buf[MAX_BUF_SIZE];
 };
-
+// used to pass info from uprobe to uretprobe via the bufs buffer
 struct ssl_state {
     __u64 buf;
     __u64 ssl;
 };
 
+// for client/server ip and port to be in ssl_buf
 struct conn_info {
     __u32 client_ip;
     __u16 client_port;
@@ -38,10 +40,11 @@ struct conn_info {
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 10240);
-    __type(key, __u32);
+    __type(key, __u32); // tid
     __type(value, struct ssl_state);
 } bufs SEC(".maps");
 
+// hashmaps to obtain socket info and associate it to a sslpt
 struct {
     __uint(type, BPF_MAP_TYPE_HASH);
     __uint(max_entries, 10240);
@@ -63,11 +66,13 @@ struct {
     __type(value, struct conn_info);
 } ssl_to_conn SEC(".maps");
 
+// ringbuffer that the go daemon will read from
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
     __uint(max_entries, 8192 * 128);
 } ringbuf SEC(".maps");
 
+// only capture nginx traffic
 static __always_inline int is_target(void) {
     char comm[16];
     bpf_get_current_comm(&comm, sizeof(comm));
@@ -75,18 +80,22 @@ static __always_inline int is_target(void) {
            comm[3] == 'n' && comm[4] == 'x' && comm[5] == '\0';
 }
 
+// probes to store plaintext and key by tid
 SEC("uprobe/SSL_read")
 int BPF_UPROBE(ssl_read_entry, void *ssl, void *buf, int num) {
     if (!is_target())
         return 0;
 
+    // tid = the id of the thread which is pid but naming it as tid
     __u64 pid_tgid = bpf_get_current_pid_tgid();
     __u32 tid = (__u32)pid_tgid;
 
+    // store sslstate in buffer for later use
     struct ssl_state s;
     s.buf = (__u64)buf;
     s.ssl = (__u64)ssl;
 
+    // store state in hashmap
     bpf_map_update_elem(&bufs, &tid, &s, BPF_ANY);
     return 0;
 }
@@ -126,33 +135,39 @@ int BPF_UPROBE(ssl_free_entry, void *ssl) {
     return 0;
 }
 
+// exit probes to obtain plaintext from hash buffer along with connection info`
 SEC("uretprobe/SSL_read")
 int BPF_URETPROBE(ssl_read_exit) {
     __u64 pid_tgid = bpf_get_current_pid_tgid();
     __u32 tid = (__u32)pid_tgid;
     __u32 pid = pid_tgid >> 32;
 
+    // obtain associated buffer pointer
     struct ssl_state *s = bpf_map_lookup_elem(&bufs, &tid);
     if (!s)
         return 0;
 
+    // get the return value of the symbol
     long ret = (int)PT_REGS_RC(ctx);
     if (ret <= 0) {
         bpf_map_delete_elem(&bufs, &tid);
         return 0;
     }
 
+    // prevent copying beyond buffer size
     __u32 len = (__u32)ret;
     if (len > MAX_BUF_SIZE - 1)
         len = MAX_BUF_SIZE - 1;
     len &= (MAX_BUF_SIZE - 1);
 
+    // reserve space for sslevent
     struct ssl_buf *e =
         bpf_ringbuf_reserve(&ringbuf, sizeof(struct ssl_buf), 0);
     if (!e) {
         bpf_map_delete_elem(&bufs, &tid);
         return 0;
     }
+    // package the ssl buffer event and submit to ringbuffer
     e->timens = bpf_ktime_get_ns();
     e->pid = pid;
     e->tid = tid;
@@ -161,8 +176,7 @@ int BPF_URETPROBE(ssl_read_exit) {
     e->ssl_ptr = (__u64)s->ssl;
     bpf_probe_read_user(e->buf, len, (void *)s->buf);
 
-    bpf_printk("lookup ssl=%llx", e->ssl_ptr);
-
+    // obtain connection info
     struct conn_info *c = bpf_map_lookup_elem(&ssl_to_conn, &e->ssl_ptr);
     if (c) {
         e->client_ip = c->client_ip;
@@ -171,16 +185,13 @@ int BPF_URETPROBE(ssl_read_exit) {
         e->server_port = bpf_ntohs(c->server_port);
     } else {
 
-        bpf_printk("ssl lookup failed");
         e->client_ip = 0;
         e->client_port = 0;
         e->server_ip = 0;
         e->server_port = 0;
     }
 
-    bpf_printk("read_exit client=%x:%u server=%x:%u", e->client_ip,
-               e->client_port, e->server_ip, e->server_port);
-
+    // submit to ringbuffer and delete in hash
     bpf_ringbuf_submit(e, 0);
     bpf_map_delete_elem(&bufs, &tid);
     return 0;
@@ -235,20 +246,24 @@ int BPF_URETPROBE(ssl_write_exit) {
         e->server_port = 0;
     }
 
-    bpf_printk("write_exit client=%x:%u server=%x:%u", e->client_ip,
-               e->client_port, e->server_ip, e->server_port);
     bpf_ringbuf_submit(e, 0);
     bpf_map_delete_elem(&bufs, &tid);
     return 0;
 }
 
+// use to key connection info by tid
 SEC("kretprobe/inet_csk_accept")
 int BPF_KRETPROBE(inet_csk_accept_exit, struct sock *sk) {
+    // process only for nginx content
+    if (!is_target())
+        return 0;
+
     if (!sk)
         return 0;
 
     __u32 tid = (__u32)bpf_get_current_pid_tgid();
 
+    // use CORE macro to read from socket
     struct conn_info conn = {};
     conn.client_ip = BPF_CORE_READ(sk, __sk_common.skc_daddr);
     conn.client_port = BPF_CORE_READ(sk, __sk_common.skc_dport);
@@ -263,8 +278,12 @@ int BPF_KRETPROBE(inet_csk_accept_exit, struct sock *sk) {
     return 0;
 }
 
+// use tid to obtain connection info and key by file descriptor instead
 SEC("kretprobe/__x64_sys_accept")
 int BPF_KRETPROBE(accept_exit) {
+    if (!is_target())
+        return 0;
+
     __u32 tid = (__u32)bpf_get_current_pid_tgid();
 
     int newfd = PT_REGS_RC(ctx);
@@ -276,13 +295,16 @@ int BPF_KRETPROBE(accept_exit) {
         return 0;
 
     bpf_map_update_elem(&fd_to_conn, &newfd, conn, BPF_ANY);
-    bpf_printk("fd=%d stored", newfd);
+    // delete elem after user
     bpf_map_delete_elem(&tid_to_conn, &tid);
     return 0;
 }
 
 SEC("kretprobe/__x64_sys_accept4")
 int BPF_KRETPROBE(accept4_exit) {
+    if (!is_target())
+        return 0;
+
     __u32 tid = (__u32)bpf_get_current_pid_tgid();
 
     int newfd = PT_REGS_RC(ctx);
@@ -294,20 +316,23 @@ int BPF_KRETPROBE(accept4_exit) {
         return 0;
 
     bpf_map_update_elem(&fd_to_conn, &newfd, conn, BPF_ANY);
-    bpf_printk("fd=%d stored", newfd);
     bpf_map_delete_elem(&tid_to_conn, &tid);
     return 0;
 }
 
+// finally map sslptr -> connection info
 SEC("uprobe/SSL_set_fd")
 int BPF_UPROBE(ssl_set_fd, void *ssl, int fd) {
+
     struct conn_info *conn = bpf_map_lookup_elem(&fd_to_conn, &fd);
     if (!conn)
         return 0;
 
     __u64 ssl_key = (__u64)ssl;
+
+    // map will be used by read/write exit probes to obtain client/server ip and
+    // port
     bpf_map_update_elem(&ssl_to_conn, &ssl_key, conn, BPF_ANY);
-    bpf_printk("ssl=%llx fd=%d mapped", ssl_key, fd);
     bpf_map_delete_elem(&fd_to_conn, &fd);
     return 0;
 }
