6633ff37bac9a2b6d7a09075a73c300922e26cf9
Uttam1916 <tired3120@gmail.com>
2026-07-15 16:21:41 +0530

    feat: add descision based on detections

---
+++ internal/analysis/policy.go
@@ -0,0 +1,40 @@
+package analysis
+
+type ClientState struct {
+	Score int
+}
+
+var clients = make(map[string]*ClientState)
+
+type Action int
+
+const (
+	Log Action = iota
+	Block
+)
+
+func Decide(clientIP string, detections []Detection) Action {
+	// nothing detected
+	if len(detections) == 0 {
+		return Log
+	}
+
+	// create state if first time seeing this client
+	state, ok := clients[clientIP]
+	if !ok {
+		state = &ClientState{}
+		clients[clientIP] = state
+	}
+
+	// add this request's score
+	for _, det := range detections {
+		state.Score += det.Score
+	}
+
+	// decide action
+	if state.Score >= 90 {
+		return Block
+	}
+
+	return Log
+}
