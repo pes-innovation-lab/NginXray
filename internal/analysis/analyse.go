b445043afcea710bd495ff0cd8516546263354c2
Uttam1916 <tired3120@gmail.com>
2026-07-15 14:46:54 +0530

    refactor: detector & add cmdi detect

--- internal/analysis/analyse.go
+++ internal/analysis/analyse.go
@@ -27,6 +27,13 @@ type Detection struct {
 	Score int
 }
 
+type Detector func(RequestContext, parser.HTTPRequest) *Detection
+
+var detectors = []Detector{
+	detectSQLi,
+	detectCmdi,
+}
+
 // common sql patterns to check against
 var sqlPatterns = []string{
 	"union select",
@@ -36,6 +43,43 @@ var sqlPatterns = []string{
 	"sleep(",
 	"benchmark(",
 	"waitfor delay",
+}
+
+var cmdPatterns = []string{
+	// Shell operators
+	";",
+	"&&",
+	"||",
+	"|",
+	"`",
+	"$(",
+
+	// Common commands
+	"bash",
+	"sh",
+	"cmd.exe",
+	"powershell",
+	"wget",
+	"curl",
+	"nc",
+	"netcat",
+	"python",
+	"perl",
+
+	// File/system commands
+	"cat /etc/passwd",
+	"cat /etc/shadow",
+	"whoami",
+	"id",
+	"uname -a",
+	"ls",
+	"pwd",
+
+	// Dangerous commands
+	"rm -rf",
+	"chmod",
+	"chown",
+	"mkfifo",
 }
 
 // maybe something we can look at later
@@ -48,15 +92,16 @@ var sqlPatterns = []string{
 func AnalyseReq(req parser.HTTPRequest, ctx RequestContext) []Detection {
 	var detections []Detection
 
-	if det := detectSQLi(req, ctx); det != nil {
-		detections = append(detections, *det)
+	for _, detector := range detectors {
+		if det := detector(ctx, req); det != nil {
+			detections = append(detections, *det)
+		}
 	}
-
 	return detections
 }
 
 // for detecting sql injections
-func detectSQLi(req parser.HTTPRequest, ctx RequestContext) *Detection {
+func detectSQLi(ctx RequestContext, req parser.HTTPRequest) *Detection {
 	// for better string operations
 	var input strings.Builder
 
@@ -88,5 +133,37 @@ func detectSQLi(req parser.HTTPRequest, ctx RequestContext) *Detection {
 		}
 	}
 
+	return nil
+}
+
+func detectCmdi(ctx RequestContext, req parser.HTTPRequest) *Detection {
+	var input strings.Builder
+
+	input.WriteString(req.Path)
+	input.WriteString(" ")
+
+	input.Write(req.Body)
+
+	data := strings.ToLower(input.String())
+
+	// decode if needed
+	if decoded, err := url.QueryUnescape(data); err == nil {
+		data = decoded
+	}
+
+	for _, pattern := range cmdPatterns {
+		if strings.Contains(data, pattern) {
+			return &Detection{
+				Timestamp:  ctx.Timestamp,
+				ClientIP:   ctx.ClientIP,
+				Method:     req.Method,
+				Path:       req.Path,
+				AttackType: "Command Injection",
+				Pattern:    pattern,
+				// will think of a proper implementation for score later
+				Score: 90,
+			}
+		}
+	}
 	return nil
 }
