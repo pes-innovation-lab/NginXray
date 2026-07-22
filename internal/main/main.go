6f48b32a67808aab01031178fbefe44bc9f062f0
Sarah <sarahkazi12806@gmail.com>
2026-07-22 11:17:21 +0530

    fix:add testblockip to trie

--- internal/main/main.go
+++ internal/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 	filter "nginxray/internal/filter"
 	sniffer "nginxray/internal/sniffer"
 )
@@ -15,6 +16,7 @@ func main() {
 	defer fw.Close()
 
 	fw.StartGC()
+	fw.AddBlocked(filter.TestBlockIP, 5*time.Minute, filter.BLOCK_THREATFEED)
 
 	sniffer.Main(fw)
 }
